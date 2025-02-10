8c6b3aa990c2cdf359e4cda4a483a7e6f3adce89
memorytao <memorytao@gmail.com>
2025-02-10 19:30:45 +0700

    Committing local changes before pulling updates

--- log_path.py
+++ log_path.py
@@ -4,20 +4,19 @@ LOG_CONTACT_PATH = "/opt/link/logs/trans_log/contact_history"
 
 
 TRUE_SERVER = {
-    "TRUE_AGW01":"192.168.1.135",
-"TRUE_AGW02":"192.168.1.135",
-"TRUE_AGW03":"192.168.1.135",
-"TRUE_AGW04":"192.168.1.135",
+"TRUE_AGW01":{"host":"192.168.1.135","port":8081},
+"TRUE_AGW02":{"host":"192.168.1.135","port":8082}
+# "TRUE_AGW03":"192.168.1.135",
+# "TRUE_AGW04":"192.168.1.135",
 }
 
 
 DTAC_SERVER = {
-"DTAC_AGW01":"192.168.1.135" ,
-"DTAC_AGW02":"192.168.1.135",
-"DTAC_AGW03":"192.168.1.135",
-"DTAC_AGW04":"192.168.1.135"
+"DTAC_AGW01":{"host":"192.168.1.135","port":8083} ,
+"DTAC_AGW02":{"host":"192.168.1.135","port":8084},
+# "DTAC_AGW03":"192.168.1.135",
+# "DTAC_AGW04":"192.168.1.135"
 }
-
 
 user_name = "danateq"
 password = "DTN@danateq202401"
--- test/test.py
+++ test/test.py
@@ -6,4 +6,4 @@ pattern = r'^[^:]+:(.*)'
 match = re.match(pattern, text)
 if match:
     result = match.group(1)
-    print(result)
+    print(result) 
